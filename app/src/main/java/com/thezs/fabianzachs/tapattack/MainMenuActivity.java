4c4c8701e5a2568b7b39536c70ebbec18fc35bc3
Fabian Zachs <fabianzachs@gmail.com>
2018-01-29 00:04:35 +0000

    functionality of fullscreen remains. TODO: REFACTOR CODE

--- app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenuActivity.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenuActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,21 +28,35 @@ public class MainMenuActivity extends AppCompatActivity {
     private ArrayList<MediaPlayer> mediaPlayers; // these players loop -> turn of onStop()
     private SharedPreferences prefs;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
 
         mediaPlayers = new ArrayList<MediaPlayer>();
         prefs = getSharedPreferences("playerPrefs", MODE_PRIVATE);
 
-        // to make app fullscreen
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        makeFullscreen();
 
         setContentView(R.layout.activity_main_menu);
         initMusic(R.raw.mainmenu);
+    }
+
+    private void makeFullscreen() {
+        // task bar
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     // shows the settings alert dialog
@@ -77,6 +93,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+
         dbuilder.setView(alertView);
         final AlertDialog dialog = dbuilder.create();
 
@@ -95,8 +112,19 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+        // dialog fullscreen
+        //Set the dialog to not focusable (makes navigation ignore us adding the window)
+        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
         dialog.show();
+        //Set the dialog to immersive
+        dialog.getWindow().getDecorView().setSystemUiVisibility(
+                this.getWindow().getDecorView().getSystemUiVisibility());
+
+//Clear the not focusable flag from the window
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
     }
+
+
 
     // plays music
     private void initMusic(int sound) {
@@ -159,6 +187,20 @@ public class MainMenuActivity extends AppCompatActivity {
         repeatMpResume();
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            this.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
     // when app is closed
     @Override
     protected void onStop() {
@@ -167,7 +209,7 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void playButton(View view) {
-        Intent intent = new Intent(this, PlayActivity.class);
+        Intent intent = new Intent(this, Play2Activity.class);
         intent.putExtra("gamemode", "classic");
         startActivity(intent);
     }
