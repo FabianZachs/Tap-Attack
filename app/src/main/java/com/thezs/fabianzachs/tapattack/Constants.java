72271e8ad0c8ae77e00adcb9070d9565fc775c86
Fabian Zachs <fabianzachs@gmail.com>
2018-02-06 15:10:16 +0000

    added constants

---
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Constants.java
@@ -0,0 +1,13 @@
+package com.thezs.fabianzachs.tapattack;
+
+/**
+ * Created by fabianzachs on 06/02/18.
+ */
+
+public class Constants {
+    public static int SCREEN_HEIGHT;
+    public static int SCREEN_WIDTH;
+    public static int SHAPE_HEIGHT;
+    public static int SHAPE_WIDTH;
+    public static int SHAPE_SPACING;
+}
