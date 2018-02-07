6549381c2d0868fe7c7ab98a251c5376d530ec55
Fabian Zachs <fabianzachs@gmail.com>
2018-02-07 16:28:23 +0000

    implimented some ShapeObject methods/ attributes

--- app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapeObject.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapeObject.java
@@ -1,8 +1,36 @@
 package com.thezs.fabianzachs.tapattack.Game.GameObjects;
 
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
 /**
  * Created by fabianzachs on 07/02/18.
  */
 
-public interface ShapeObject {
+public abstract class ShapeObject {
+
+    private float durationAlive;
+    private Rect bitmapHolder;  // rectangle to hold bitmap ?? needed??
+    private int points;
+    private long initTime;
+    private String color;  // will be used to find correct bitmap img
+
+
+    public abstract void draw(Canvas canvas);
+    public abstract void update();
+
+    private boolean isAlive() {
+        // if time alive > durationAlive, object should be removed
+        return !(System.currentTimeMillis() - initTime > durationAlive);
+    }
+
+
+
+
+
+
+
+
+
 }
