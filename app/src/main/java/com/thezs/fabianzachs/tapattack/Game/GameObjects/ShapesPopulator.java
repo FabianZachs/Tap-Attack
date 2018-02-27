6c897de49b57ea7ac2fd132a697a71ac0c23b8e7
Fabian Zachs <fabianzachs@gmail.com>
2018-02-27 14:10:07 +0000

    receiving java.util.ConcurrentModificationException error since we change shapes while we are iterating over them. redo implemetation.

--- app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
@@ -1,10 +1,13 @@
 package com.thezs.fabianzachs.tapattack.Game.GameObjects;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.Log;
 
 import com.thezs.fabianzachs.tapattack.Animation.AnimationManager;
+import com.thezs.fabianzachs.tapattack.Constants;
 import com.thezs.fabianzachs.tapattack.Game.GameObjects.Shapes.ShapeObject;
 import com.thezs.fabianzachs.tapattack.Game.GameObjects.Shapes.Square;
 
@@ -22,10 +25,14 @@ public class ShapesPopulator {
     private final int SHAPE_SPACING = 5; // space between shapes
 
     private long initTime;
+    private Rect newShapeArea; // instead of creating a new rect for every shape we want to create
+    private Random rand;
     private ShapeBuilder shapeBuilder;
 
     public ShapesPopulator(long initTime) {
         this.initTime = initTime;
+        this.newShapeArea = new Rect(300,300,300,300);
+        rand = new Random(); // TODO make attribute?
         this.shapeBuilder = new ShapeBuilder(initTime);
     }
 
@@ -46,25 +53,29 @@ public class ShapesPopulator {
 
     // TODO find the right bounds for location for shape
     private Point getValidNewShapeLocation(ArrayList<ShapeObject> shapes) {
-        Random rand = new Random();
 
         int i = rand.nextInt(1000) + 100;
         int j = rand.nextInt(1000) + 200;
 
-        while(locationUsedByAnotherShape(shapes,i,j)) {
+        /*while(locationUsedByAnotherShape(shapes,i,j)) {
 
             i = rand.nextInt(1000) + 100;
             j = rand.nextInt(1000) + 200;
 
-        }
+        }*/
         return new Point(i,j);
 
     }
 
     // TODO add SHAPE_SPACING???
     private boolean locationUsedByAnotherShape(ArrayList<ShapeObject> shapes, int i, int j) {
+        //Rect newShapeArea = new Rect(i - Constants.SCREEN_WIDTH - this.SHAPE_SPACING,j - Constants.SHAPE_HEIGHT - this.SHAPE_SPACING,i + Constants.SCREEN_WIDTH + this.SHAPE_SPACING, j + Constants.SHAPE_HEIGHT + this.SHAPE_SPACING);
+        this.newShapeArea.left = i - Constants.SCREEN_WIDTH - this.SHAPE_SPACING;
+        this.newShapeArea.top = j - Constants.SHAPE_HEIGHT - this.SHAPE_SPACING;
+        this.newShapeArea.right = i + Constants.SCREEN_WIDTH + this.SHAPE_SPACING;
+        this.newShapeArea.bottom = j + Constants.SHAPE_HEIGHT + this.SHAPE_SPACING;
         for (ShapeObject shape : shapes) {
-            if (shape.getBitmapHolder().contains(i,j))
+            if (Rect.intersects(shape.getBitmapHolder(), this.newShapeArea))
                 return true;
         }
         return false;
