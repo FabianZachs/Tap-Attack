7fad5d82346f5c2a44242001dfd9526da13e5a59
Fabian Zachs <fabianzachs@gmail.com>
2018-03-03 09:58:18 +0000

    done with first chunk of GraveFactory. Pt 2 after finishing Arrow shape

---
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GraveObjects/ArrowGrave.java
@@ -0,0 +1,15 @@
+package com.thezs.fabianzachs.tapattack.Game.GraveObjects;
+
+import com.thezs.fabianzachs.tapattack.Game.GameObjects.Shapes.ShapeObject;
+
+/**
+ * Created by fabianzachs on 03/03/18.
+ */
+
+public class ArrowGrave extends GraveObject {
+
+
+    public ArrowGrave(ShapeObject shapeToCreateGraveFrom) {
+        super(shapeToCreateGraveFrom);
+    }
+}
