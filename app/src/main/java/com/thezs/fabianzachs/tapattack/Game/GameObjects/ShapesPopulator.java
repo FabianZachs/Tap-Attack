53269783081cf1f9a23e27be2178cf37bb70fbf1
Fabian Zachs <fabianzachs@gmail.com>
2018-03-07 12:54:41 +0000

    done with warning color for now, next: implementing a set of paint that will be resused over and over instead of initializing new paint for each ShapeObject

--- app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
@@ -70,7 +70,7 @@ public class ShapesPopulator {
 
         // TODO use factory design pattern? so instead of .buildCross, pass "cross" in parameter
         //mShapes.add(shapeBuilder.buildArrow("blue", newShapeLocation));
-        ShapeObject newShape = shapeBuilder.buildShape("circle",getColor(), newShapeLocation,"LEFT") ;
+        ShapeObject newShape = shapeBuilder.buildShape("arrow",/*getColor()*/ "blue", newShapeLocation,"LEFT") ;
         newShape.attachAllObservers(scoreObserver,streakObserver,progressBarObserver);
         shapes.add(newShape);
         timeOfLastShapeAddition = System.currentTimeMillis();
