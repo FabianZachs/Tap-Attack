b3b5486c0f272b7bd7291112bd05be546d5712f4
Fabian Zachs <fabianzachs@gmail.com>
2018-03-08 20:30:26 +0000

    created mediator class & package

---
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/Mediator/CentralGameCommunication.java
@@ -0,0 +1,13 @@
+package com.thezs.fabianzachs.tapattack.Game.Mediator;
+
+/**
+ * Created by fabianzachs on 08/03/18.
+ */
+
+public class CentralGameCommunication {
+
+    /* Tasks:
+    * recieve messages regarding destruction of shape, incorrect taps of shape, (passing required info)
+    *
+     */
+}
