c9257b4e7b5cb41b3eef9ac09adcefa11baf2606
Fabian Zachs <fabianzachs@gmail.com>
2018-03-08 13:40:23 +0000

    previously mentioned bug can not be reproduced

--- app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/Game/GameObjects/ShapesPopulator.java
@@ -85,7 +85,7 @@ public class ShapesPopulator {
 
         // TODO use factory design pattern? so instead of .buildCross, pass "cross" in parameter
         //mShapes.add(shapeBuilder.buildArrow("blue", newShapeLocation));
-        ShapeObject newShape = shapeBuilder.buildShape("circle",/*getColor()*/"blue", newShapeLocation,paint,bitmapHolder,"LEFT") ;
+        ShapeObject newShape = shapeBuilder.buildShape("circle", getColor(), newShapeLocation,paint,bitmapHolder,"LEFT") ;
         newShape.attachAllObservers(scoreObserver,streakObserver,progressBarObserver);
         shapes.add(newShape);
         timeOfLastShapeAddition = System.currentTimeMillis();
