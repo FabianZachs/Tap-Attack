f77265a1630222c61c779851f81c15bebc72be1f
Fabian Zachs <fabianzachs@gmail.com>
2018-08-24 16:11:10 +0200

    new main menu restructure complete

--- app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenu/Menu/PaidUnlockSection/PaidItemType.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenu/Menu/PaidUnlockSection/PaidItemType.java
@@ -33,7 +33,7 @@ public abstract class PaidItemType extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_more_points2, container, false);
+        View view = inflater.inflate(R.layout.fragment_paid_items, container, false);
         adapter.setListener(new PaidUnlockAdapter.PaidUnlockAdapterListener() {
             @Override
             public void purchaseComplete() {
--- app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenu/Menu/PaidUnlockSection/PaidUnlocksFragment.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/MainMenu/Menu/PaidUnlockSection/PaidUnlocksFragment.java
@@ -36,7 +36,7 @@ public class PaidUnlocksFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_more_points3, container, false);
+        View view = inflater.inflate(R.layout.fragment_paid_menu, container, false);
 
 
         ImageView backbutton = view.findViewById(R.id.back_image);
