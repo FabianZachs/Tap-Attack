dfb1f5340befad4472d6f4bfcd77b28a5530179e
Fabian Zachs <fabianzachs@gmail.com>
2018-08-29 13:36:28 +0200

    gravebuilder created

--- app/src/main/java/com/thezs/fabianzachs/tapattack/GameFragment/ShapeObjects/GraveObjects/GraveShapeBuilder.java
+++ app/src/main/java/com/thezs/fabianzachs/tapattack/GameFragment/ShapeObjects/GraveObjects/GraveShapeBuilder.java
@@ -1,4 +1,21 @@
 package com.thezs.fabianzachs.tapattack.GameFragment.ShapeObjects.GraveObjects;
 
+import com.thezs.fabianzachs.tapattack.GameFragment.ShapeObjects.NormalShapes.Arrow;
+import com.thezs.fabianzachs.tapattack.GameFragment.ShapeObjects.NormalShapes.Circle;
+import com.thezs.fabianzachs.tapattack.GameFragment.ShapeObjects.NormalShapes.ShapeObject;
+
 public class GraveShapeBuilder {
+
+    public static GraveObject buildGrave(ShapeObject shape) {
+
+        if (shape instanceof Circle) {
+            return new CircleGrave((Circle) shape);
+        }
+
+        else if (shape instanceof Arrow) {
+            return new ArrowGrave((Arrow) shape);
+        }
+
+        throw new IllegalArgumentException("unknown grave to build");
+    }
 }
